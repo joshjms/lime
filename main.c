69df1c9c53ad14dd3c64b357851ba786e1edf568
joshjms <joshjms1607@gmail.com>
2026-01-31 14:55:54 +0800

    add version cmd
    Signed-off-by: joshjms <joshjms1607@gmail.com>

--- main.c
+++ main.c
@@ -3,6 +3,9 @@
 
 #include "run.h"
 
+const char *VERSION = "0.1.0";
+const char *AUTHORS = "Joshua James";
+
 static void print_usage(const char *prog) {
     fprintf(stderr, "Lime - A lightweight rootless container runtime\n");
 }
@@ -10,6 +13,9 @@ static void print_usage(const char *prog) {
 int main(int argc, char **argv) {
     if (argc > 1 && strcmp(argv[1], "run") == 0) {
         return handle_run(argc, argv);
+    } else if(argc > 1 && strcmp(argv[1], "version") == 0) {
+        printf("Lime v%s by %s\n", VERSION, AUTHORS);
+        return 0;
     }
 
     print_usage(argv[0]);
